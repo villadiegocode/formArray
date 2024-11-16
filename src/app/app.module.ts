8c12d0c50f9b7f2bc9c1dcedfcb09acfb07f6257
villadiegocode <carlovinar93@gmail.com>
2024-11-15 21:02:14 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from '@components/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CardModule} from "primeng/card";
+import {ChipsModule} from "primeng/chips";
+import {Button} from "primeng/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CardModule,
+    ReactiveFormsModule,
+    ChipsModule,
+    Button,
   ],
   providers: [],
   bootstrap: [AppComponent]
